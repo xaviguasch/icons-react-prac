d2b9914d401795b08c75a2451e8c3e770d5844f6
xaviguasch <xaviguasch@gmail.com>
2019-04-30 20:12:31 +0200

    starter files

---
+++ src/IconList.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react'
+
+class IconList extends Component {
+  static defaultProps = {
+    options: [
+      'angry',
+      'anchor',
+      'archive',
+      'at',
+      'archway',
+      'baby',
+      'bell',
+      'bolt',
+      'bone',
+      'car',
+      'city',
+      'cloud',
+      'couch'
+    ]
+  }
+  constructor(props) {
+    super(props)
+    this.state = { icons: [] }
+    this.addIcon = this.addIcon.bind(this)
+  }
+
+  addIcon() {
+    let idx = Math.floor(Math.random() * this.props.options.length)
+    let newIcon = this.props.options[idx]
+  }
+
+  render() {
+    const icons = this.state.icons.map(i => <i className={`fas fa-${i}`} />)
+    return (
+      <div>
+        <h1>Icons: {icons}</h1>
+        <button onClick={this.addIcon}>Add New Icon</button>
+      </div>
+    )
+  }
+}
+
+export default IconList
